e882e2c5bfa4c20f34e2b756ec407247aabd3414
Sajid Ali <30510036+s-sajid-ali@users.noreply.github.com>
2019-03-01 09:00:56 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.core import Extension
 from Cython.Build import build_ext
+from Cython.Build import cythonize
 import numpy as np
 
 module1=[ Extension('basic_cython',
@@ -8,11 +9,13 @@ module1=[ Extension('basic_cython',
           directives = {'linetrace': True, 'language_level': 3},
           language='c')]
 
+extensions = cythonize(module1)
+
 if __name__ == "__main__":
      setup( name = 'cython_test_coverage',
             packages=['cython_test_coverage'],
             cmdclass = {'build_ext': build_ext},
-            ext_modules = module1,
+            ext_modules = extensions,
             include_dirs=[np.get_include()],
             description='cython coverage test',
             url='https://github.com/s-sajid-ali/cython_test_coverage',
